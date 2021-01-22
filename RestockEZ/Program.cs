b3d9e72fdf4c2167173ddcfb62e4c97a52a61d96
mimt88 <77353903+mimt88@users.noreply.github.com>
2021-01-22 09:42:27 -0800

    Moofie patched the database, but i found another way to get in so I'm adding that in.

--- RestockEZ/Program.cs
+++ RestockEZ/Program.cs
@@ -7,6 +7,8 @@ namespace RestockEZ
         static void Main(string[] args)
         {
             System.Diagnostics.Process.Start("explorer.exe", "https://alts.top");
+            Console.WriteLine("Lmao broke ass stop complaining about a free service, if you want something that consistently restocks pay for it. \n L");
+            Console.ReadKey();
         }
     }
 }
